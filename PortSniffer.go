dfd1609a18519f77d6b12f455072f4211bbcada8
Max Struijs <maxstruys32@gmail.com>
2023-03-03 19:46:54 +0100

    Added comments

--- PortSniffer.go
+++ PortSniffer.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
+	//Ref to the port directory
 	"github.com/ChronoMax/PortSniffer/port"
 )
 
 func main() {
 	fmt.Println("Port Scanner..")
 
+	//Difining the var open as the return value of the func ScanPort in the port directory.
 	open := port.ScanPort("tcp", "localhost", 80)
+	//Prints the text with the set value.
 	fmt.Printf("Port open: %t\n", open)
 }
